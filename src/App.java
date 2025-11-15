f55e93d368012bb0bd76e13578d73347cfb647c1
LucKsz001 <pedrolucas19souza@gmail.com>
2025-11-15 17:28:31 -0300

    mensagem de bem vindo

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Iae, meu brother!");
+        System.out.println("Bem vindos! ao meu projeto em Java");
     }
 }
